a69aafbef600689aaa645679ab71def90eb55e0c
Yiran Zhang <Zhang_Yiran@outlook.com>
2018-03-17 00:35:32 -0400

    + models

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MainList(models.Model):
+    category = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Todo(models.Model):
+    text = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField(null=True)
+    is_finished = models.BooleanField(default=False)
+    title = models.ForeignKey(MainList, on_delete=models.CASCADE)
+
+
+
+
